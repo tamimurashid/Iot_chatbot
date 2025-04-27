828000620184967584893ab6bbafdec69e2a722e
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-04-28 01:08:37 +0300

    update

---
+++ backend/nlp_engine.py
@@ -0,0 +1,15 @@
+from sklearn.metrics.pairwise import cosine_similarity  
+from response import *
+from flask import  jsonify
+
+# NLP response
+def get_best_reply(user_message):
+    user_embedding = model.encode(user_message)
+    similarities = cosine_similarity([user_embedding], phrase_embeddings)[0]
+    best_idx = np.argmax(similarities)
+
+    reply = reply_keys[best_idx] if similarities[best_idx] > 0.6 else \
+            "❌ Sorry, I didn’t understand that. Type 'help' to see valid commands."
+
+    return reply
+
